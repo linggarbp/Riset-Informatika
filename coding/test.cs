c0288f70e1b5b91d148ca786f3968e7a90c0a5ee
Linggar Bhakti Pratama <90260272+linggarbp@users.noreply.github.com>
2023-12-25 08:43:13 +0700

    Update test.cs

--- coding/test.cs
+++ coding/test.cs
@@ -1,1 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class DatasetTester : MonoBehaviour
+{
+    public TextAsset dataset;
+
+    void Start()
+    {
+        string[] lines = dataset.text.Split('\n');
+        foreach (string line in lines)
+        {
+            Debug.Log(line);
+        }
+    }
+}
+
+using NUnit.Framework;
+using UnityEngine;
+
+public class ExampleTest
+{
+    [Test]
+    public void ExampleTestSimplePasses()
+    {
+        // Arrange
+        int a = 1;
+        int b = 2;
+
+        // Act
+        int result = a + b;
+
+        // Assert
+        Assert.AreEqual(3, result);
+    }
+}
